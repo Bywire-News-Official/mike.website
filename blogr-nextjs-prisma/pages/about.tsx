6fc96b1ccf31d232de6e6b4bae07fe1b1b6365ce
heavymossman <michael@nizamimedia.com>
2023-07-01 20:46:29 +0100

    feat: Add unique title and meta info, set p width in About page
    - Integrated react-helmet to add unique title and meta description to the About page
    - Set a maximum width for paragraph text to approximately 70 characters for better readability

--- blogr-nextjs-prisma/pages/about.tsx
+++ blogr-nextjs-prisma/pages/about.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
 
 const About: React.FC = () => {
   return (
     <Layout>
+      <Helmet>
+        <title>About Us | Michael O'Sullivan</title>
+        <meta name="description" content="We are a team of passionate developers dedicated to creating high-quality software solutions. Our mission is to provide our clients with the best possible products and services, while continuously improving our skills and staying up-to-date with the latest technologies." />
+      </Helmet>
       <div>
         <h1>About Us</h1>
         <p>
--- blogr-nextjs-prisma/pages/index.tsx
+++ blogr-nextjs-prisma/pages/index.tsx
@@ -3,40 +3,19 @@ import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 
-export const getStaticProps: GetStaticProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Welcome to my portfolio website",
-      content: "Hello World! My name is Michael O'Sullivan and this is my website.",
-      published: true,
-      author: {
-        name: "Michael O'Sullivan",
-        email: "mo@bywire.news",
-      },
-    },
-  ]
-  return { 
-    props: { feed }, 
-    revalidate: 10 
-  }
-}
+
 
 type Props = {
   feed: PostProps[]
 }
 
-const Blog: React.FC<Props> = (props) => {
+const Home: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>Public Feed</h1>
+        <h1>QUOTE</h1>
         <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
+         
         </main>
       </div>
       <style jsx>{`
@@ -57,4 +36,4 @@ const Blog: React.FC<Props> = (props) => {
   )
 }
 
-export default Blog
+export default Home
