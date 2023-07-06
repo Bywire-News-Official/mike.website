15b1f340566500efc19a2b2eb2e5aad27c2a27db
heavymossman <michael@nizamimedia.com>
2023-07-06 23:20:25 +0100

    feat: Add JavaScript encoded email address to Contact component for spam protection

--- blogr-nextjs-prisma/pages/contact.tsx
+++ blogr-nextjs-prisma/pages/contact.tsx
@@ -6,6 +6,25 @@ import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg
 
 const Contact: React.FC = () => {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    setEmail(["michael", "bywire.news"].join("@"));
+
+    const script = document.createElement('script');
+
+    script.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    window["BrevoConversationsID"] = "64a72ff73bfa944e0430f046";
+    window['BrevoConversations'] = window['BrevoConversations'] || function() { (window['BrevoConversations'].q = window['BrevoConversations'].q || []).push(arguments); };
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setForm({ ...form, [e.target.id]: e.target.value });
@@ -16,22 +35,6 @@ const Contact: React.FC = () => {
     console.log(form);
     // Here you would typically send the form data to your server
   };
-
-  useEffect(() => {
-    const script = document.createElement('script');
-
-    script.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    window["BrevoConversationsID"] = "64a72ff73bfa944e0430f046";
-window['BrevoConversations'] = window['BrevoConversations'] || function() { (window['BrevoConversations'].q = window['BrevoConversations'].q || []).push(arguments); };
-
-    return () => {
-      document.body.removeChild(script);
-    }
-  }, []);
 
   return (
     <Layout>
@@ -44,19 +47,19 @@ window['BrevoConversations'] = window['BrevoConversations'] || function() { (win
         <div className="row">
           <div className="col-md-6">
             <p>
-              Get in touch or shoot me an email directly on <strong>michael@bywire.news</strong>
+              Get in touch or shoot me an email directly on{" "}
+              <a href={`mailto:${email}`}><strong>{email}</strong></a>
             </p>
-           
           </div>
         </div>
         <div className="social-icons mt-3">
-          <a className="m-2" href="https://twitter.com/username" target="_blank" rel="noopener noreferrer">
+          <a className="m-2" href="https://twitter.com/bywirenews" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faTwitter} size="1x" />
           </a>
-          <a className="m-2" href="https://facebook.com/username" target="_blank" rel="noopener noreferrer">
+          <a className="m-2" href="https://facebook.com/heavymossman" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFacebook} size="1x" />
           </a>
-          <a className="m-2" href="https://instagram.com/username" target="_blank" rel="noopener noreferrer">
+          <a className="m-2" href="https://instagram.com/ukfactcheckpolitics" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faInstagram} size="1x" />
           </a>
         </div>
