a3d5feb08c9c456994bd89762353783bb2958522
heavymossman <michael@nizamimedia.com>
2023-07-06 18:46:20 +0100

    updated aboutbk page

--- blogr-nextjs-prisma/pages/aboutbk.tsx
+++ blogr-nextjs-prisma/pages/aboutbk.tsx
@@ -1,44 +1,39 @@
 import React from "react";
 import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBolt, faVoteYea, faCogs, faCode } from '@fortawesome/free-solid-svg-icons'
+
 
 const About: React.FC = () => {
   const content = [
     {
-      title: "Artificial Intelligence",
-      description: "Your description here...",
-      link: "https://github.com/yourGithubProfile",
-      linkText: "View on Github"
-    },
-    {
-      title: "Product Invention",
-      description: "Your description here...",
+      title: "Product Innovation and Management",
+      description: "As the CEO of Bywire news, I've brought blockchain and cryptocurrency technologies to revolutionise the news media landscape. In addition, I am a specialist in optimising existing processes and encourage innovation whenever I can, which proved critical when launching Disrupt and Refine.",
       link: "https://yourProductLink.com",
-      linkText: "View Products"
+      linkText: "View Products",
+      icon: <FontAwesomeIcon icon={faBolt} />,
     },
     {
-      title: "Media",
-      description: "Your description here...",
-      link: "https://dribbble.com/yourDribbbleProfile",
-      linkText: "View Dribbble"
-    },
-    {
-      title: "Politics",
-      description: "Your description here...",
+      title: "Political Campaigns",
+      description: "I’ve contributed to winning political strategies with my innovative digital campaigning tactics, including psychometric micro-targeting, Machine Learning optimisation and audience demographic segmentation. This includes successful campaigns for Vote Leave and Labour Leave.",
       link: "#",
-      linkText: "View More"
+      linkText: "View More",
+      icon: <FontAwesomeIcon icon={faVoteYea} />,
     },
     {
-      title: "My Playlist",
-      description: "Your playlist description here...",
+      title: "Leadership and Management",
+      description: "As a leader, my principles are built on collaboration, innovation, and commitment to creating a difference. I have led teams to phenomenal growth by constructing a strategic vision, encouraging innovation and fostering a results-driven culture.",
       link: "https://open.spotify.com/playlist/yourPlaylist",
-      linkText: "View Playlist"
+      linkText: "View Playlist",
+      icon: <FontAwesomeIcon icon={faCogs} />,
     },
     {
-      title: "My Writings",
-      description: "Your writings description here...",
+      title: "Engineering (AI, Python, Front-end)",
+      description: "My foundation in programming, Python and front-end development has driven me to disrupt traditional approaches in software and systems engineering. I've built competencies in fields like Machine Learning, Artificial Intelligence, data analytics, Blockchain and Cryptography.",
       link: "https://yourWritingsLink.com",
-      linkText: "Visit Page"
+      linkText: "Visit Page",
+      icon: <FontAwesomeIcon icon={faCode} />,
     }
   ];
 
@@ -47,15 +42,14 @@ const About: React.FC = () => {
       <Helmet>
         <title>About Me | Michael O'Sullivan</title>
         <meta name="description" content="I am a seasoned programmer with the unique ability to tackle intricate technical challenges while crafting websites that exude sleekness and visual allure. I am also a product manager and a designer." />
-      </Helmet>
+        </Helmet>
       <div className="page megaMargin p-3 whitebk">
-        <h1>About Me</h1>
+        <h1 className="mb-5">About Me</h1>
 
         {content.map((item, index) => (
           <div className="row" key={index}>
             <div className="col-1">
-              {/* Replace with your icon */}
-              <i className="icon-example"></i>
+              {item.icon} 
             </div>
             <div className="col-8">
               <h3>{item.title}</h3>
