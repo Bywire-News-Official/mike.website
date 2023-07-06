34fdc4cc67b1c5b99cb93a7e3305905e3c76121b
heavymossman <michael@nizamimedia.com>
2023-07-06 19:47:49 +0100

    align homepage items

--- blogr-nextjs-prisma/pages/index.tsx
+++ blogr-nextjs-prisma/pages/index.tsx
@@ -30,106 +30,119 @@ const Home: React.FC<Props> = (props) => {
 
   return (
     <Layout>
-      <div className="page">
-        <div className="row whitebk">
-          <div className="col-md-6">
-            <h1>{content.title}</h1>
-            {content.description.map((paragraph, index) => (
-              <p className="fp" key={index}>{paragraph}</p>
-            ))}
-            <p className="my-3">
-              <Link href="/about"><a>{content.learn}</a></Link>
-            </p>
-            <div className="social-media-icons">
-              <a href={content.socialMediaLinks.linkedin} target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faLinkedin} size="1x" />
-              </a>
-              <a href={content.socialMediaLinks.facebook} target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faFacebook} size="1x" />
-              </a>
-              <a href={content.socialMediaLinks.twitter} target="_blank" rel="noreferrer">
-                  <FontAwesomeIcon icon={faTwitter} size="1x" />
-              </a>
-            </div>
-            <div className="bitcoin-sign">
-                {content.bitcoinSign}
-                <FontAwesomeIcon icon={faBitcoin} size="1x" />
-            </div>
-          </div>
-          <div className="col-12 col-md-6">
-            <div className="imageContainer">
-              <Image
-                src="/eos.png"
-                alt="Projects and Clients"
-                layout='fill'
-                objectFit='contain'
-                objectPosition='right'
-              />
-            </div>
+    <div className="page">
+      <div className="row whitebk">
+        <div className="col-md-5">
+          <h1>{content.title}</h1>
+          {content.description.map((paragraph, index) => (
+            <p className="fp" key={index}>{paragraph}</p>
+          ))}
+          <p className="my-3">
+            <Link href="/about"><a>{content.learn}</a></Link>
+          </p>
+          <div className="row align-items-center">
+  <div className="col-auto">
+    <div className="social-media-icons">
+      <a href="https://www.linkedin.com/in/heavymossman/" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="1x" />
+      </a>
+      <a href="https://www.facebook.com/HeavyMossMan/" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faFacebook} size="1x" />
+      </a>
+      <a href="https://twitter.com/bywirenews" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={faTwitter} size="1x" />
+      </a>
+    </div>
+  </div>
+  <div className="col-auto">
+    <div className="bitcoin-sign">
+        {content.bitcoinSign}
+        <FontAwesomeIcon icon={faBitcoin} size="1x" />
+    </div>
+  </div>
+</div></div>
+        <div className="col-12 col-md-7">
+          <div className="imageContainer">
+            <Image
+              src="/eos.png"
+              alt="Projects and Clients"
+              layout='fill'
+              objectFit='contain'
+              objectPosition='right'
+            />
           </div>
         </div>
       </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-
-        .social-media-icons {
+    </div>
+    <style jsx>{`
+      /* rest of the styles */
+      .bitcoin-sign {
           display: flex;
-          justify-content: flex-start;
-          gap: 20px;
-          margin-top: 20px;
-        }
-
-        .bitcoin-sign {
-          display: flex;
-          justify-content: flex-start;
           align-items: center;
-          margin-top: 20px;
-          color: #f7931a; /* Bitcoin's color */
+          color: #f7931a;
           font-size: 16px;
           font-weight:bold;
-        }
-        .inlineIcons {
-          display: flex;
-  justify-content: space-between; /* Adjusts the space between the children elements, you can change as per your need */
-        }
-        @keyframes typewriter {
-          0% { width: 0; }
-          100% { width: 100%; }
-        }
+          margin-left: 20px;
+      }
+.post {
+        background: white;
+        transition: box-shadow 0.1s ease-in;
+      }
 
-        .typewriter-text {
-          border-right: 2px solid;
-          white-space: nowrap;
-          overflow: hidden;
-          animation: typewriter 4s steps(44) 0s 1 normal both,
-                     blinkTextCursor 500ms steps(44) 0s 1 normal both;
-        }
+      .post:hover {
+        box-shadow: 1px 1px 3px #aaa;
+      }
 
-        @keyframes blinkTextCursor {
-          0% {border-color: transparent;}
-          50% {border-color: transparent;}
-          100% {border-color: black;}
-        }
+      .post + .post {
+        margin-top: 2rem;
+      }
 
-        .imageContainer {
-          position: relative;
-          width: 100%;
-          height: 500px;
-        }
-      
-      `}</style>
-    </Layout>
+      .social-media-icons {
+        display: flex;
+        justify-content: flex-start;
+        gap: 20px;
+        margin-top: 20px;
+      }
+
+      .bitcoin-sign {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin-top: 20px;
+        color: #f7931a; /* Bitcoin's color */
+        font-size: 16px;
+        font-weight:bold;
+      }
+      .inlineIcons {
+        display: flex;
+justify-content: space-between; /* Adjusts the space between the children elements, you can change as per your need */
+      }
+      @keyframes typewriter {
+        0% { width: 0; }
+        100% { width: 100%; }
+      }
+
+      .typewriter-text {
+        border-right: 2px solid;
+        white-space: nowrap;
+        overflow: hidden;
+        animation: typewriter 4s steps(44) 0s 1 normal both,
+                   blinkTextCursor 500ms steps(44) 0s 1 normal both;
+      }
+
+      @keyframes blinkTextCursor {
+        0% {border-color: transparent;}
+        50% {border-color: transparent;}
+        100% {border-color: black;}
+      }
+
+      .imageContainer {
+        position: relative;
+        width: 100%;
+        height: 500px;
+      }
+    `}</style>
+  </Layout>
   )
 }
 
