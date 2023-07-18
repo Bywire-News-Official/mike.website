86fda296fa26babf03cb2ba42c963010e91b4bf0
heavymossman <michael@nizamimedia.com>
2023-07-18 20:43:20 +0100

    mobile responsive intro

--- blogr-nextjs-prisma/components/LoadingScreen.tsx
+++ blogr-nextjs-prisma/components/LoadingScreen.tsx
@@ -15,7 +15,7 @@ const LoadingScreen = ({ setLoadingDone }) => {
     "Click anywhere or hit Enter to start!"
   ];
   const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
-  const [text, setText] = useState("");
+  const [displayedMessages, setDisplayedMessages] = useState([]);
   const [charIndex, setCharIndex] = useState(0);
 
   useEffect(() => {
@@ -23,22 +23,20 @@ const LoadingScreen = ({ setLoadingDone }) => {
       return;
     }
 
-    if (messages[currentMessageIndex] === 'Clear') {
-      setText("");
-      setCurrentMessageIndex(currentMessageIndex + 1);
-      setCharIndex(0);
-      return;
-    }
+    const timeoutId = setTimeout(() => {
+      setCharIndex((prevIndex) => prevIndex + 1);
+    }, 120);
 
-    if (charIndex < messages[currentMessageIndex].length) {
-      const timeoutId = setTimeout(() => {
-        setText((text) => text + messages[currentMessageIndex][charIndex]);
-        setCharIndex(charIndex + 1);
-      }, 120);
+    return () => clearTimeout(timeoutId);
+  }, [charIndex, currentMessageIndex]);
 
-      return () => clearTimeout(timeoutId);
-    } else {
-      setText((text) => text + '<br /><br />');
+  useEffect(() => {
+    if (charIndex === messages[currentMessageIndex]?.length) {
+      if (messages[currentMessageIndex] === 'Clear') {
+        setDisplayedMessages([]);
+      } else {
+        setDisplayedMessages(oldArray => [...oldArray, messages[currentMessageIndex]]);
+      }
       setCurrentMessageIndex(currentMessageIndex + 1);
       setCharIndex(0);
     }
@@ -66,29 +64,52 @@ const LoadingScreen = ({ setLoadingDone }) => {
 
   return (
     <div className="loading-screen">
-      <pre className="typewriter" dangerouslySetInnerHTML={{ __html: text }} />
+      <pre className="typewriter">
+        {displayedMessages.map((msg, index) => (
+          <span key={index}>{msg}<br/><br/></span>
+        ))}
+        {messages[currentMessageIndex]?.slice(0, charIndex)}
+      </pre>
       <style jsx>{`
         .loading-screen {
           background-color: black;
           color: green;
           height: 100vh;
+          width: 100vw;
           display: flex;
           justify-content: center;
           align-items: center;
+          padding: 2rem;
+          box-sizing: border-box;
           cursor: ${currentMessageIndex >= messages.length ? 'pointer' : 'default'};
           font-size: 2rem;
         }
 
         .typewriter {
           text-align: center;
+          white-space: pre-wrap;
           border-right: .15em solid green;
           animation: blink-caret .75s step-end infinite;
+          max-width: 100%;
+          overflow-wrap: break-word;
         }
 
         /* The typewriter cursor effect */
         @keyframes blink-caret {
           from, to { border-color: transparent }
           50% { border-color: green; }
+        }
+
+        @media (max-width: 600px) {
+          .loading-screen {
+            font-size: 1.5rem;
+          }
+        }
+
+        @media (max-width: 400px) {
+          .loading-screen {
+            font-size: 1rem;
+          }
         }
       `}</style>
     </div>
