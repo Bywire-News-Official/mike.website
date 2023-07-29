039cf36b9b4c1a83687f1130eaa137bc6e80e546
heavymossman <michael@nizamimedia.com>
2023-07-29 21:04:57 +0100

    ssr changes

--- blogr-nextjs-prisma/pages/_document.tsx
+++ blogr-nextjs-prisma/pages/_document.tsx
@@ -9,13 +9,13 @@ class MyDocument extends Document {
           <Script
             src="https://plausible.io/js/script.js"
             data-domain="mike.website"
-            strategy="beforeInteractive"
+            strategy="afterInteractive"
           />
           {/* Add the highlight.js library */}
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/styles/default.min.css" />
           <Script 
             src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/highlight.min.js" 
-            strategy="beforeInteractive"
+            strategy="afterInteractive"
           />
         </Head>
         <body>
