b015a7c3626f02bea6bde9f79349e02806b099e3
heavymossman <michael@nizamimedia.com>
2023-07-29 21:38:05 +0100

    basic meta information to the homepage

--- blogr-nextjs-prisma/pages/index.tsx
+++ blogr-nextjs-prisma/pages/index.tsx
@@ -5,6 +5,8 @@ import Layout from "../components/Layout"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFacebook, faTwitter, faLinkedin, faBitcoin } from "@fortawesome/free-brands-svg-icons"
 import Link from "next/link"
+import Head from 'next/head'
+
 
 const Home = () => {
     const [loadingDone, setLoadingDone] = useState(false);
@@ -75,7 +77,13 @@ const Home = () => {
     }
 
     return (
+        
         <Layout>
+             <Head>
+        <title>Mike's Website</title>
+        <meta name="description" content="This is Michael O'Sullivan, the founder and CEO of Bywire News, personal website. He loves technology, innovation, and Python programming." />
+        <meta name="keywords" content="Michael O'Sullivan, Bywire, UK Fact Check Politics, Technology, Innovation, Python, Programming" />
+      </Head>
             <div className="">
                 <div className="row whitebk">
                     <div className=" col-12 col-lg-6 col-md-12">
